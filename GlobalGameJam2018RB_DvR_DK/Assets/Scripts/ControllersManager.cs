651c7f4eb8fb4d44ab90a9c6beeab795dc9f76fb
David_Katsma <davidlskatsma@gmail.com>
2018-01-24 22:36:41 +0100

    Added xbox controller plugin. Made 2 simple classes. One Controller manager which gets all controllers available and updates them One ControllerControl class which will handle all inputs. Events will be written which can be hooked on by outside classes. Makes it easier to map custom events.

---
+++ GlobalGameJam2018RB_DvR_DK/Assets/Scripts/ControllersManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using XInputDotNetPure;
+
+public class ControllersManager : MonoBehaviour
+{
+	private ControllerControl[] _controllers;
+
+	private void Start()
+	{
+		_controllers = new ControllerControl[4];
+		GetControllers();
+	}
+
+	private void Update()
+	{
+		GetControllers();
+		UpdateControllers();
+	}
+
+	private void GetControllers()
+	{
+		for (int i = 0; i < _controllers.Length; i++)
+		{
+			if (_controllers[i] == null && GamePad.GetState((PlayerIndex)i).IsConnected)
+			{
+				Debug.Log(string.Format("Player {0}  Connected", i));
+
+				_controllers[i] = new ControllerControl
+				{
+					ControllerIndex = (PlayerIndex)i,
+				};
+			}
+		}
+	}
+
+	private void UpdateControllers()
+	{
+		for (int i = 0; i < _controllers.Length; i++)
+		{
+			if (_controllers[i] != null)
+			{
+				_controllers[i].Update();
+			}
+		}
+	}
+}
