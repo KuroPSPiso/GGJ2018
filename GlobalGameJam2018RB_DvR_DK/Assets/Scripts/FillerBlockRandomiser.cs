27202692f53ec441ba6f635820b1a5dbe1718b48
David Katsma <davidlskatsma@gmail.com>
2018-01-27 15:17:28 +0100

    made filler blocks have random colours. First level is in and can be playtested.

---
+++ GlobalGameJam2018RB_DvR_DK/Assets/Scripts/FillerBlockRandomiser.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FillerBlockRandomiser : MonoBehaviour {
+
+    [SerializeField]
+    private Material[] _materials;
+
+    private Renderer[] _childrenRenderers;
+
+	// Use this for initialization
+	void Start () {
+
+        _childrenRenderers = new MeshRenderer[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            _childrenRenderers[i] = transform.GetChild(i).GetComponent<Renderer>();
+        }
+
+        ScrambleColours();
+	}
+	
+    private void ScrambleColours()
+    {
+        for(int i = 0; i < _childrenRenderers.Length; i++)
+        {
+            _childrenRenderers[i].material = _materials[Random.Range(0, _materials.Length)];
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
