f798a3124d7491edbfe18559e5da26aaf61d3cd5
Daan <daan@gfheu.org>
2018-01-28 14:40:01 +0100

    Added winning state

--- GlobalGameJam2018RB_DvR_DK/Assets/Scripts/Objects/EndCentre.cs
+++ GlobalGameJam2018RB_DvR_DK/Assets/Scripts/Objects/EndCentre.cs
@@ -6,28 +6,30 @@ public class EndCentre : Hook
     public int packageType;
     public Transform tankFill;
     float score = 0.0f;
+    public PlayerManager playerManager;
 
-	public float maxScore = 3;
+    public float maxScore = 3;
+    bool winning = false;
 
     void Update()
     {
-        if (tankFill.localScale.z < score * 13)
+        if (tankFill.localScale.z < score * 13 && !winning)
         {
             Vector3 scale = tankFill.localScale;
-            scale.z += Time.deltaTime;
+            scale.z = Mathf.Min(13, scale.z + Time.deltaTime * 2.0f);
             tankFill.localScale = scale;
 
             if (scale.z >= score * 13)
             {
-                //TODO:
-                Debug.Log("winning team is " + packageType);
+                playerManager.GotoWin(packageType);
+                winning = true;
             }
         }
     }
 
     public void IncreaseData()
     {
-		score += 1.0f / maxScore; 
+        score += 1.0f / maxScore;
         AudioManager.TankFilled();
     }
 }
