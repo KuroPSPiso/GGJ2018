36a1d8648597ba8428011c6087ff554f4480ccbb
David Katsma <davidlskatsma@gmail.com>
2018-01-28 13:57:15 +0100

    Updated Terminal Made Player Stun particle

---
+++ GlobalGameJam2018RB_DvR_DK/Assets/Scripts/RotatorControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatorControl : MonoBehaviour {
+
+	public Vector3 RotationSpeed;
+
+	// Update is called once per frame
+	void Update () {
+
+		var currentRotation = transform.localEulerAngles;
+		currentRotation += RotationSpeed * Time.deltaTime;
+		transform.localEulerAngles = currentRotation;
+	}
+}
